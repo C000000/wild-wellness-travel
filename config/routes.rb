7f0bf7b3b439905323ac12f7872c98db9b0a6057
David Jokhadze <djokhadze@gmail.com>
2016-12-15 22:30:12 +0400

    added user details and retreats

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#landing'
-  get 'home' => 'pages#home'
+  root to: 'pages#home'
+  get 'landing' => 'pages#landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212172621) do
+ActiveRecord::Schema.define(version: 20161215181707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "retreats", force: :cascade do |t|
+    t.string   "company_name"
+    t.string   "country"
+    t.string   "city"
+    t.string   "state"
+    t.string   "street_address"
+    t.string   "phone_number"
+    t.string   "email"
+    t.string   "video"
+    t.decimal  "price"
+    t.text     "description"
+    t.boolean  "available",      default: true
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "date_of_birth"
+    t.string   "picture"
+    t.string   "phone_number"
+    t.string   "gender"
+    t.string   "country"
+    t.string   "city"
+    t.string   "state"
+    t.string   "street_address"
+    t.string   "zip_code"
+    t.boolean  "owner",                  default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
