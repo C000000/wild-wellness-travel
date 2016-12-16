73448a347f6c7dd29580a16ae8e35657d3e135c4
David Jokhadze <djokhadze@gmail.com>
2016-12-16 19:34:43 +0400

    added edit page

---
+++ app/assets/javascripts/user_profile.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".tab").on("click", function(e){
+		$(".tab-content").hide();
+		$(".tab").removeClass("active");
+		$($(this).data("target")).fadeIn(1000);
+		$(this).addClass("active");
+	});
+});
