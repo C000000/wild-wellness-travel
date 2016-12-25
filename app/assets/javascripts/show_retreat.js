6884c4bf3b6545bcfc0f01efc9e3d9a09842945a
David Jokhadze <djokhadze@gmail.com>
2016-12-25 23:23:46 +0400

    fixed navbar-mobile and footer

--- app/assets/javascripts/show_retreat.js
+++ app/assets/javascripts/show_retreat.js
@@ -7,9 +7,11 @@ $(document).ready(function(){
   });
 
   $(".retreat-pictures > div:first").fadeIn(300);
-  setInterval(function() {
-  $(".retreat-pictures > div:first").hide("slide", { direction: "left" }, 500)
-  .next().show("slide", { direction: "right" }, 500)
-  .end().appendTo(".retreat-pictures");
-  },  4000);
+  if ($(".retreat-pictures div").length > 1) {
+    setInterval(function() {
+      $(".retreat-pictures > div:first").hide("slide", { direction: "left" }, 500)
+      .next().show("slide", { direction: "right" }, 500)
+      .end().appendTo(".retreat-pictures");
+    },  4000);
+  }
 });
